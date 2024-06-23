a9dc8d889dfec38ee33f5797e69d9b702b48d257
21C <twentyonec21c@gmail.com>
2024-06-23 23:30:31 +0530

    Centered fullscreen UI

--- src/main/java/com/twentyonec/Plutus/FXMLController.java
+++ src/main/java/com/twentyonec/Plutus/FXMLController.java
@@ -7,11 +7,39 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class FXMLController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private VBox mainVBox;
+
+    @FXML
+    public void initialize() {
+        // Add listeners to dynamically update the layout positions
+        anchorPane.widthProperty().addListener((observable, oldValue, newValue) -> {
+            mainVBox.setLayoutX((newValue.doubleValue() - mainVBox.getWidth()) / 2);
+        });
+
+        anchorPane.heightProperty().addListener((observable, oldValue, newValue) -> {
+            mainVBox.setLayoutY((newValue.doubleValue() - mainVBox.getHeight()) / 2);
+        });
+
+        mainVBox.widthProperty().addListener((observable, oldValue, newValue) -> {
+            mainVBox.setLayoutX((anchorPane.getWidth() - newValue.doubleValue()) / 2);
+        });
+
+        mainVBox.heightProperty().addListener((observable, oldValue, newValue) -> {
+            mainVBox.setLayoutY((anchorPane.getHeight() - newValue.doubleValue()) / 2);
+        });
+    }
 
     @FXML
     private void handleRoomButtonAction(ActionEvent event) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.twentyonec {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+	requires javafx.graphics;
     opens com.twentyonec.Plutus to javafx.fxml;
     exports com.twentyonec.Plutus;
 }
